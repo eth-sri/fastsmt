633c414c00e53aa56777dac5bfebedc953fc7292
Mislav Balunovic <bmislav@student.ethz.ch>
2018-11-30 15:40:40 +0100

    learning.py to synthesis.py in experiment scripts

--- fastsmt/experiments/runners/run_core.sh
+++ fastsmt/experiments/runners/run_core.sh
@@ -1,2 +1,2 @@
-python synthesis/synthesis.py experiments/configs/core/config_apprentice.json --benchmark_dir experiments/data/qf_bv/bruttomesso/core --max_timeout 10 --num_iters 10 --iters_inc 10 --pop_size 1 --eval_dir experiments/eval/core/ --smt_batch_size 300 --full_pass 10 --num_threads 80 --experiment_name core_apprentice
+python synthesis/learning.py experiments/configs/core/config_apprentice.json --benchmark_dir experiments/data/qf_bv/bruttomesso/core --max_timeout 10 --num_iters 10 --iters_inc 10 --pop_size 1 --eval_dir experiments/eval/core/ --smt_batch_size 300 --full_pass 10 --num_threads 80 --experiment_name core_apprentice
 
